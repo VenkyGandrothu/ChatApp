8c51708050d9af1f06bbba6d825a28c17776ec73
Venky10D <venky@10dds.in>
2026-06-17 15:03:24 +0530

    started registration api creation

---
+++ Backend/src/main/java/com/chatapp/www/dto/RegistrationDTO.java
@@ -0,0 +1,17 @@
+package com.chatapp.www.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationDTO {
+    private String name;
+    private String email;
+    private String password;
+}
--- Backend/src/main/java/com/chatapp/www/entity/user.java
+++ Backend/src/main/java/com/chatapp/www/entity/user.java
@@ -1,10 +1,28 @@
 package com.chatapp.www.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class user {
-            
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+
+    @Column(name = "name")
     private String name;
+    @Column(name = "email")
     private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 
     
     public Long getId() {
@@ -26,6 +44,12 @@ public class user {
     public void setEmail(String email) {
         this.email = email;
     }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }   
 
 
     public user(Long id, String name, String email) {
