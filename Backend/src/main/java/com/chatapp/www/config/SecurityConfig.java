6839c43fbc2145b19a9495fd396f62179f7dd182
VenkyGandrothu <venky@10dds.in>
2026-06-18 17:54:52 +0530

    JWT services created

--- Backend/src/main/java/com/chatapp/www/config/SecurityConfig.java
+++ Backend/src/main/java/com/chatapp/www/config/SecurityConfig.java
@@ -29,3 +29,4 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
+ 
--- Backend/src/main/java/com/chatapp/www/security/JwtService.java
+++ Backend/src/main/java/com/chatapp/www/security/JwtService.java
@@ -1,8 +1,14 @@
 package com.chatapp.www.security;
 
+import java.security.Key;
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import lombok.Value;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtService {
@@ -13,4 +19,50 @@ public class JwtService {
 
     @Value("${jwt.expiration}")
     private long expirationTime;
+
+    //convert secret key to signing key 
+    private Key getSigningKey(){
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    //generate token
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    //extract username from token
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    //extract expiration from token
+    public Date extractExpiration(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+    }
+
+    //check if token is expired
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    //check if token is valid
+    public boolean isTokenValid(String token, String email) {
+        return (extractUsername(token).equals(email) && !isTokenExpired(token));
+    }
 }
